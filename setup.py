788c44dbe1d33078f0489213eabc6da8e3aeeb86
Krishna Yadav <krishna.yadav@plateiq.com>
2020-04-21 18:53:21 +0530

    updated wait_for_angular method

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
 setup(
     name='pytractor-new',
 
-    version='0.1.3',
+    version='0.1.4',
 
     description='Selenium testing for Angular.js apps',
     long_description=long_description,
